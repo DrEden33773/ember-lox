d598c7f57811665fd9051ef8eb2703419cfb650d
DrEden33773 <edwardwang33773@gmail.com>
2025-01-23 00:05:14 +0800

    test 1

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,34 @@ use std::fs;
 use std::io::{self, Write};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
+  let args: Vec<String> = env::args().collect();
+  if args.len() < 3 {
+    eprintln!("Usage: {} tokenize <filename>", args[0]);
+    return;
+  }
+
+  let command = &args[1];
+  let filename = &args[2];
+
+  match command.as_str() {
+    "tokenize" => {
+      // You can use print statements as follows for debugging, they'll be visible when running tests.
+      eprintln!("Logs from your program will appear here!");
+
+      let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+        eprintln!("Failed to read file {}", filename);
+        String::new()
+      });
+
+      // Uncomment this block to pass the first stage
+      if !file_contents.is_empty() {
+        panic!("Scanner not implemented");
+      } else {
+        println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+      }
     }
-
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
-
-            // Uncomment this block to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            // }
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
-        }
+    _ => {
+      eprintln!("Unknown command: {}", command);
     }
+  }
 }
